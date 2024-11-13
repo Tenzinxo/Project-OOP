549c1ca5f0e2e0cdd3bee9c8ce111e1e15c5d157
existenzin <89390232+Tenzinxo@users.noreply.github.com>
2024-11-14 00:55:49 +0100

    Main File

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Tashi Delek Tsangmalo!");
+		System.out.println("This is a Project given to me for my Java Object Oriented Programming Class.");
+		System.out.println("The Goal is to make a java application that can manage,analyze and extract metadata from an image file.");
+		System.out.println("Yinta!!!");
+	}
+
+}
